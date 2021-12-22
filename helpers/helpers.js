7950ff8fb12e9c9b8d4277c5a54855228cee734b
Olya <olichka@MacBook-Air-Olya.local>
2021-12-22 20:27:25 +0200

    fixed validation for less then 3 items

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -8,7 +8,7 @@ function validateUserInput (input) {
   if (!input.length) {
     console.log('Whoops, you\'ve entered nothing. Try again!')
   } else if (input.length < 3) {
-    console.log('Not enough steps. Should be more then 3')
+    console.log('Not enough steps. Should be 3 or more')
   } else if (input.length % 2 === 0) {
     console.log('You sould enter odd number of steps')
   } else if (input.filter((el, i) => input.indexOf(el) !== i).length >= 1) {
